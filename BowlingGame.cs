d840777fcffa2c8fac103769355de422990e3315
Chris Elsbree <chrise@iconics.com>
2023-01-27 10:53:37 -0500

    F!! Two frames
    Co-Authored-By: Mellidium <17202886+Mellidium@users.noreply.github.com>
    Co-Authored-By: ReniKIcon17 <109752214+renikicon17@users.noreply.github.com>
    Co-Authored-By: Marco Tagliaferri <123575170+marco-tagliaferri@users.noreply.github.com>
    Co-Authored-By: jhp151 <119470052+jhp151@users.noreply.github.com>
    Co-Authored-By: alexKlimov781 <123010130+alexKlimov781@users.noreply.github.com>
    Co-Authored-By: iconics-rich <66848248+iconics-rich@users.noreply.github.com>
    Co-Authored-By: Josh Obal <46050567+milfordjosh@users.noreply.github.com>

--- BowlingGame.cs
+++ BowlingGame.cs
@@ -1,3 +1,5 @@
+using ApprovalUtilities.Utilities;
+
 namespace Bowling;
 
 public class BowlingGame
@@ -6,12 +8,12 @@ public class BowlingGame
 
     public void Roll(params int[] rolls)
     {
-        _frames.Add(new Frame(rolls[0], rolls[1]));
-
+        _frames.Add(new Frame(1, 0, rolls[0], rolls[1]));
+        _frames.Add(new Frame(2, _frames.Last().TotalScore, rolls[2], rolls[3]));
     }
 
     public override string ToString()
     {
-        return _frames.First().ToString();
+        return _frames.JoinWith("\n");
     }
 }
--- Frame.cs
+++ Frame.cs
@@ -4,18 +4,22 @@ public class Frame
 {
     private int roll1;
     private int roll2;
+    private int frameIndex;
+    private int PreviousScore;
 
-    public Frame(int roll1, int roll2)
+    public Frame(int frameIndex, int previousScore, int roll1, int roll2)
     {
         this.roll1 = roll1;
         this.roll2 = roll2;
+        this.frameIndex = frameIndex;
+        this.PreviousScore = previousScore;
     }
 
-    public int TotalScore => FrameScore;
+    public int TotalScore => FrameScore + PreviousScore;
     public int FrameScore => roll1 + roll2;
 
-    public string ToString()
+    public override string ToString()
     {
-        return $"Frame 1: {roll1}, {roll2} ; FrameScore: {FrameScore} ; TotalScore: {TotalScore}";
+        return $"Frame {frameIndex}: {roll1}, {roll2} ; FrameScore: {FrameScore} ; TotalScore: {TotalScore}";
     }
 }
