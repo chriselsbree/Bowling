f4b97bb91e6835275b116fbdb914a24a092c1b4b
Chris Elsbree <chrise@iconics.com>
2023-01-27 11:09:50 -0500

    F!! Three Frames
    Co-Authored-By: Mellidium <17202886+Mellidium@users.noreply.github.com>
    Co-Authored-By: ReniKIcon17 <109752214+renikicon17@users.noreply.github.com>
    Co-Authored-By: Marco Tagliaferri <123575170+marco-tagliaferri@users.noreply.github.com>
    Co-Authored-By: jhp151 <119470052+jhp151@users.noreply.github.com>
    Co-Authored-By: alexKlimov781 <123010130+alexKlimov781@users.noreply.github.com>
    Co-Authored-By: iconics-rich <66848248+iconics-rich@users.noreply.github.com>
    Co-Authored-By: Josh Obal <46050567+milfordjosh@users.noreply.github.com>

--- BowlingGame.cs
+++ BowlingGame.cs
@@ -10,6 +10,8 @@ public class BowlingGame
     {
         _frames.Add(new Frame(1, 0, rolls[0], rolls[1]));
         _frames.Add(new Frame(2, _frames.Last().TotalScore, rolls[2], rolls[3]));
+        _frames.Add(new Frame(3, _frames.Last().TotalScore, rolls[4], rolls[5]));
+
     }
 
     public override string ToString()
