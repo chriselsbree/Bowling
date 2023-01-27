1d385019dc5d9c316590dd0334289520757757b7
Chris Elsbree <chrise@iconics.com>
2023-01-27 10:12:17 -0500

    F!! Rolls
    Co-Authored-By: Mellidium <17202886+Mellidium@users.noreply.github.com>
    Co-Authored-By: ReniKIcon17 <109752214+renikicon17@users.noreply.github.com>
    Co-Authored-By: Marco Tagliaferri <123575170+marco-tagliaferri@users.noreply.github.com>
    Co-Authored-By: jhp151 <119470052+jhp151@users.noreply.github.com>
    Co-Authored-By: alexKlimov781 <123010130+alexKlimov781@users.noreply.github.com>
    Co-Authored-By: iconics-rich <66848248+iconics-rich@users.noreply.github.com>
    Co-Authored-By: Josh Obal <46050567+milfordjosh@users.noreply.github.com>

--- BowlingGame.cs
+++ BowlingGame.cs
@@ -2,13 +2,17 @@ namespace Bowling;
 
 public class BowlingGame
 {
-    public void Roll(params int[] rolls )
-    {
+    private int roll1;
+    private int roll2;
 
+    public void Roll(params int[] rolls)
+    {
+        roll1 = rolls[0];
+        roll2 = rolls[1];
     }
 
     public override string ToString()
     {
-        return "Frame 1";
+        return $"Frame 1: {roll1}, {roll2}";
     }
 }
