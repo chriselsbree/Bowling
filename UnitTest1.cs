1d4924ab9b3c00149a62e806cd939d34b02341d7
Chris Elsbree <chrise@iconics.com>
2023-01-27 09:12:41 -0500

    initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using NuGet.Frameworks;
+
+namespace FizzBuzz2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
